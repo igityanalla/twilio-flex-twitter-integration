d9043d724d12f56db3971e6176a606f278231602
aigityan <igityanalla@gmail.com>
2022-07-29 16:21:11 +0200

    add conversation creation logic

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const generateTwitterToken = require('../service/twitter-security');
+const sendMessageToFlex = require('../service/flex-twitter-integration');
 
-router.post('/receive-message', async function (request, response) {
-  console.log('Received a new message from Twitter');
+// EP2: Twitter Security Check
+router.get('/receive-message', (req, res) => {
+    const crcToken = req.query.crc_token
+    const resToken = generateTwitterToken(crcToken)
 
+    res.status(200).json({response_token: resToken})
 });
 
+router.post('/receive-message', (req, res) => {
+    if (req.body.direct_message_events) {
+        console.log('Testing webhook')
+        const users = req.body.users;
+        const customer = users[Object.keys(users)[0]];
+        const twitterHandle = customer.screen_name;
+        const twitterId = customer.id;
+        // Check to make sure this is a message sent from the customer
+        // rather than a Direct Message we sent on behalf of the agent from our app
+        if (!req.body.direct_message_events[0].message_create.source_app_id) {
+            const msg =
+                req.body.direct_message_events[0].message_create.message_data.text;
+            // functions.sendMessageToFlex(twilioClient, msg, twitterHandle, twitterId);
+            sendMessageToFlex();
+        }
+    }
+    res.sendStatus(200)
+})
+
 router.post('/send-message', async function (request, response) {
-  console.log('Twilio new message webhook fired, sending back to Twitter');
-
-
+    console.log('Twilio new message webhook fired, sending back to Twitter');
 });
 
 module.exports = router;
--- service/flex-twitter-integration.js
+++ service/flex-twitter-integration.js
@@ -4,3 +4,74 @@ const client = require('twilio')(
     process.env.TWILIO_ACCOUNT_SID,
     process.env.TWILIO_AUTH_TOKEN
 );
+
+async function fetchParticipantConversations(twitterHandle) {
+    return client.conversations.participantConversations
+        .list({identity: twitterHandle});
+}
+
+async function findExistingConversation(identity) {
+    const conversations = await fetchParticipantConversations(identity);
+    let existing = conversations.find(conversation => conversation.conversationState !== 'closed');
+    console.log("Existing: ", existing);
+    return existing !== undefined ? existing.conversationSid : undefined;
+}
+
+async function createConversation(twitterHandle) {
+    return client.conversations.conversations
+        .create({
+            friendlyName: `Twitter_conversation_${twitterHandle}`
+        });
+    // TODO full name in the attributes
+}
+
+async function createParticipant(conversationSid, identity) {
+    return client.conversations.conversations(conversationSid)
+        .participants
+        .create({identity: identity});
+}
+
+async function createScopedWebhooks(conversationSid) {
+    await client.conversations.conversations(conversationSid)
+        .webhooks
+        .create({
+            'configuration.filters': 'onMessageAdded',
+            target: 'studio',
+            'configuration.flowSid': process.env.STUDIO_FLOW_SID
+        });
+
+    await client.conversations.conversations(conversationSid)
+        .webhooks
+        .create({
+            target: 'webhook',
+            'configuration.filters': 'onMessageAdded',
+            'configuration.method': 'POST',
+            'configuration.url': `${process.env.WEBHOOK_BASE_URL}/xxxxxx`,
+        })
+}
+
+async function createMessage(conversationSid, author, body) {
+    return client.conversations.conversations(conversationSid)
+        .messages
+        .create({
+            author: author,
+            body: body,
+            xTwilioWebhookEnabled: true
+        });
+}
+
+async function sendMessageToFlex(twitterHandle, body) {
+    let existingConversationSid = await findExistingConversation(twitterHandle);
+    if (existingConversationSid === undefined) {
+        const {sid: conversationSid} = await createConversation(twitterHandle);
+        console.log("Conversation SID: ", conversationSid);
+        await createParticipant(conversationSid, twitterHandle);
+        await createScopedWebhooks(conversationSid);
+        existingConversationSid = conversationSid;
+    }
+
+    const {sid: messageSid} = await createMessage(existingConversationSid, twitterHandle, body);
+    console.log("Message SID: ", messageSid);
+}
+
+module.exports = sendMessageToFlex;
